e3fa7143082e168569db10fb93663ec2e7488e80
Lauanelandim <lauane0landim@hotmail.com>
2024-11-14 20:49:57 -0300

    Update ExemploForEach.java

--- ExemploForEach.java
+++ ExemploForEach.java
@@ -5,7 +5,10 @@ public class ExemploForEach {
     public static void main(String[] args){
         int [] numeros = {1, 2, 3, 4, 5}; // Vetor inteiro "numeros".
         for (int numero : numeros) {
-    
+        /* Lado esquerdo : Lado direito
+        Lado esquerdo: Declara variável para armazenamento temporário o valor do 
+        elemento durante a repetição.
+        Lado direito: É o array/coleção que estamos percorrendo*/
             System.out.println(numero);
             
         } 
