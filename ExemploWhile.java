0600ce621ee3533ce3d5c84384b87a855b005a73
Lauanelandim <lauane0landim@hotmail.com>
2024-11-14 19:51:10 -0300

    Add files via upload

---
+++ ExemploWhile.java
@@ -0,0 +1,12 @@
+//While é utilizado quando o nº de repetições não é conhecido previamente.
+//Enquanto a condição for verdadeira, irá imprimir os números.  
+
+public class ExemploWhile {
+    public static void main(String[] args){
+        int i = 1;
+        while (i <=5) {
+            System.out.println("Número: " + i);
+            i++;
+        }
+    }
+}
