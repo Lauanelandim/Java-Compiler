4a2f9933a6310c85b194c76954789f1cdf8a225c
Lauanelandim <lauane0landim@hotmail.com>
2024-11-14 18:27:33 -0300

    Add files via upload

---
+++ Variavel.java
@@ -0,0 +1,9 @@
+public class Variavel { 
+    public static void main(String[] args) {
+        int variavel = 10;
+        String texto = "Texto"; 
+        System.out.println("O valor da variavel inteira: " + variavel);
+        System.out.println("Valor da variável texto: " + texto);
+        
+    }
+}    
