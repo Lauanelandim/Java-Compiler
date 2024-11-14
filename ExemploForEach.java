cefb966821e1a31a883c52e127f6266c1feb7b2f
Lauanelandim <lauane0landim@hotmail.com>
2024-11-14 20:37:27 -0300

    Add files via upload

---
+++ ExemploForEach.java
@@ -0,0 +1,13 @@
+// Repetição sobre arrays/ coleções.
+//Exercicio percorrendo array de inteiros.
+
+public class ExemploForEach {
+    public static void main(String[] args){
+        int [] numeros = {1, 2, 3, 4, 5}; // Vetor inteiro "numeros".
+        for (int numero : numeros) {
+    
+            System.out.println(numero);
+            
+        } 
+    }
+}
