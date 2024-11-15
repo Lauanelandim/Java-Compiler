0d9fbaab28af7431b4a03ed9471037736b4fb30c
Lauanelandim <lauane0landim@hotmail.com>
2024-11-14 21:21:46 -0300

    Add files via upload

---
+++ ExemploIfElse2.java
@@ -0,0 +1,22 @@
+/*Exercício que verifica o nível de conhecimento de programação com base nos anos 
+de experiência*/
+import java.util.Scanner;
+
+public class ExemploIfElse2 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in); /*Criar um objeto Scanner para ler a 
+        entrada do usuário.*/
+        
+        System.out.println("Digite quantos anos de experiência você possui: ");/*Solicito a 
+        entrada do usuário.*/
+        int anosDeExperiencia = scanner.nextInt();/*Lê o nº de anos de experiência.*/
+        
+        if(anosDeExperiencia <2) {
+            System.out.println("Você é um desenvolvedor júnior!");
+        } else if (anosDeExperiencia >= 2 && anosDeExperiencia <5){
+            System.out.println("Você é um desenvolvedor pleno!");
+        } else {
+            System.out.println("Você é um desenvolvedor sênior!");
+        }
+    }
+}
